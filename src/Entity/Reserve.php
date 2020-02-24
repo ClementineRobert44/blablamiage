8b7af270923007ed43c0feabf2d333d701d5bde5
unknown <clementine.robert.nantes@gmail.com>
2020-02-24 22:58:30 +0100

    Création Entity Reserve

---
+++ src/Entity/Reserve.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReserveRepository")
+ */
+class Reserve
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateReservation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateReservation(): ?\DateTimeInterface
+    {
+        return $this->dateReservation;
+    }
+
+    public function setDateReservation(\DateTimeInterface $dateReservation): self
+    {
+        $this->dateReservation = $dateReservation;
+
+        return $this;
+    }
+}
