8ca3a08fb9fe4a69605ba8e411a33cdc95af6ed5
unknown <clementine.robert.nantes@gmail.com>
2020-03-01 15:27:29 +0100

    Afficher Liste de tous les passagers

--- src/Controller/PassagerController.php
+++ src/Controller/PassagerController.php
@@ -4,17 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Passager;
+use Symfony\Component\HttpFoundation\Response;
 
 class PassagerController extends AbstractController
 {
     /**
-     * @Route("/passager", name="passager")
+     * @Route("/passager", name="passager.list")
      */
-    public function list()
+    public function list() : Response
     {
         $passagers = $this->getDoctrine()->getRepository(Passager::class)->findAll();
         return $this->render('passager/list.html.twig', [
-            'passager' => $passagers,
+            'passagers' => $passagers,
         ]);
     }
 }
